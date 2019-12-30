81103507e92ffa03e7edd56aed7bc32c21391721
Tim Stapels <tim@riskbook.com>
2019-12-30 12:20:03 +0100

    Add yesod-auth-simple-test executable
    Bumps minimum cabal version to 2.2

--- yesod-auth-simple.nix
+++ yesod-auth-simple.nix
@@ -1,20 +1,30 @@
 { mkDerivation, aeson, base, base16-bytestring, base64-bytestring
 , blaze-html, bytestring, classy-prelude, classy-prelude-yesod
 , clientsession, directory, email-validate, fast-logger, hspec
-, http-types, monad-logger, persistent, persistent-sqlite, scrypt
-, stdenv, text, time, vector, wai, yesod, yesod-auth, yesod-core
-, yesod-form, yesod-test, zxcvbn-hs
+, hspec-discover, http-types, monad-logger, persistent
+, persistent-sqlite, scrypt, stdenv, text, time, vector, wai, yesod
+, yesod-auth, yesod-core, yesod-form, yesod-test, zxcvbn-hs
 }:
 mkDerivation {
   pname = "yesod-auth-simple";
   version = "0.0.0";
   src = ./.;
+  isLibrary = true;
+  isExecutable = true;
   libraryHaskellDepends = [
     aeson base base16-bytestring base64-bytestring blaze-html
     bytestring classy-prelude classy-prelude-yesod clientsession
     email-validate http-types persistent scrypt text time vector wai
     yesod-auth yesod-core yesod-form zxcvbn-hs
   ];
+  executableHaskellDepends = [
+    aeson base base64-bytestring blaze-html bytestring classy-prelude
+    classy-prelude-yesod clientsession directory email-validate
+    fast-logger hspec http-types monad-logger persistent
+    persistent-sqlite scrypt text time vector wai yesod yesod-auth
+    yesod-core yesod-form yesod-test zxcvbn-hs
+  ];
+  executableToolDepends = [ hspec-discover ];
   testHaskellDepends = [
     aeson base base64-bytestring blaze-html bytestring classy-prelude
     classy-prelude-yesod clientsession directory email-validate
@@ -22,6 +32,7 @@ mkDerivation {
     persistent-sqlite scrypt text time vector wai yesod yesod-auth
     yesod-core yesod-form yesod-test zxcvbn-hs
   ];
+  testToolDepends = [ hspec-discover ];
   description = "Traditional email/pass auth for Yesod";
   license = stdenv.lib.licenses.bsd3;
 }
